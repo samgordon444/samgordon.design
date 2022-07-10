70e8b46eecbf7098d21cb82bebfc8be70847863b
samgordon444 <admin@samgordon.design>
2022-07-10 11:27:57 -0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <div>
+    <li v-for="post of posts" :key="post.slug">
+      <NuxtLink :to="post.slug">{{ post.title }}</NuxtLink>
+    </li>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
+  async asyncData({ $content }) {
+    const posts = await $content().fetch();
+
+    return {
+      posts,
+    };
+  },
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+    colors: {
+    },
+    fontFamily: {
+    },
+    fontWeight: {
+    },
+    extend: {
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
