cd7cbf82443badd4c92881a15d197fc807f0a8b0
samgordon444 <admin@samgordon.design>
2022-07-24 21:41:25 -0500

    removing unneeded favicon stuff

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,10 +15,8 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'stylesheet', type: 'text/css', href: 'slick/slick.css' },
-      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
+      { rel: 'stylesheet', type: 'text/css', href: 'slick/slick.css' }
     ],
     script: [
     ],
