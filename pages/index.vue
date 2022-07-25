22dfc8cf9a6a80b9c0f743470f82df366f269d38
samgordon444 <admin@samgordon.design>
2022-07-24 21:47:56 -0500

    d

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,7 @@
   <div class="px-6 lg:px-24 my-[112px] w-full">
     <Header />
     <div class="grid gap-12 lg:gap-24 grid-cols-1 lg:grid-cols-2">
-      <li v-for="post of posts.reverse()" class="list-none">
-        <!-- <NuxtLink :to="post.slug">{{ post.title }}</NuxtLink> -->
+      <!-- <li v-for="post of posts.reverse()" class="list-none">
         <div class="relative">
           <div class="absolute z-10 left-2 bottom-2 border border-dark bg-light w-16 h-16 rounded-full">
             <div class="absolute left-1/2 top-1.5 -translate-x-1/2 text-center leading-3 text-[22px]"><span class="text-lg">№.</span><br />{{ post.number }}</div>
@@ -12,7 +11,8 @@
             <img :src="post.image" class="absolute top-1/2 -translate-y-1/2 w-full" />
           </div>
         </div>
-      </li>
+      </li> -->
+      TESTING
     </div>
     <div class="pt-24">© Sam Gordon</div>
   </div>
